7381b909a61fe87e3fcc8830fe359d25fe9eae76
Keona Kirby <keonakirby@gmail.com>
2021-03-18 18:09:09 -0500

    fixed js link that did not properly load the file

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,3 @@
-//start with an individual array sets such as all caps and lowercase and characters then concat them using .concat
-
 let upperSelection = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 let lowerSelection = ["a", "b", "v", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -8,8 +6,8 @@ let numberSelection = [ 0,1, 2, 3, 4, 5, 6, 7, 8, 9 ];
 
 let specialCharSelection = ["," , "!", "#","$", "%", "&" ,"*" ,   "+", "-" , ".", "/", ":", ";" , "<" , "=", "?" , "@","^","_" ,"~"];
 // will need to concat the above list into a new variable 
-
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 //creating the generate password function
@@ -20,8 +18,10 @@ function generatePassword () {
 if( isNaN(passwordLength) || (passwordLength < 8) || (passwordLength > 128)) {
 
   alert("Try again");
-  return;
+  
 };
+
+
 let upperChoice = confirm("Would you like uppercase letters in your password? ");
 
 let lowerChoice = confirm("Would you like lowercase letters in your password? ");
@@ -30,8 +30,15 @@ let numberChoice = confirm("Would you like numbers in your password? ");
 
 let specialChoice = confirm("Would you like special characters in your password? ");
 
-}
 
+
+
+
+// for (let index = 0; index < passwordLength; index++) {
+//   const element = array[index];
+  
+// }
+}
 
 
 
