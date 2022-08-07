3d8764d19386938f05f76f509cd9d83dc8ff4e8b
Keona Kirby <keonakirbydev@gmail.com>
2022-08-06 22:37:24 -0500

    added new background and update header

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,88 @@
-let upperSelection = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let upperSelection = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
-let lowerSelection = ["a", "b", "v", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let lowerSelection = [
+  "a",
+  "b",
+  "v",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
-let numberSelection = [ 0,1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+let numberSelection = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-let specialCharSelection = ["," , "!", "#","$", "%", "&" ,"*" ,   "+", "-" , ".", "/", ":", ";" , "<" , "=", "?" , "@","^","_" ,"~"];
+let specialCharSelection = [
+  ",",
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "*",
+  "+",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  "?",
+  "@",
+  "^",
+  "_",
+  "~",
+];
 
 let passWord = "";
-// will need to concat the above list 
+// will need to concat the above list
 // Assignment Code
 
 var generateBtn = document.querySelector("#generate");
@@ -15,67 +90,58 @@ var generateBtn = document.querySelector("#generate");
 let currentUserArray = [];
 let finalArray = [];
 
-
 //creating the generate password function
-function generatePassword () {
+function generatePassword() {
+  passwordLength = prompt(
+    "Please choose a number from 8 to 128 for your password character length"
+  );
+  //nan = not a number
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Try again");
+    return;
+  }
 
-  passwordLength = prompt("Please choose a number from 8 to 128 for your password character length");
+  let upperChoice = confirm(
+    "Would you like uppercase letters in your password? "
+  );
 
-if( isNaN(passwordLength) || (passwordLength < 8) || (passwordLength > 128)) {
+  let lowerChoice = confirm(
+    "Would you like lowercase letters in your password? "
+  );
 
-  alert("Try again");
-  
-};
+  let numberChoice = confirm("Would you like numbers in your password? ");
 
+  let specialChoice = confirm(
+    "Would you like special characters in your password? "
+  );
 
-let upperChoice = confirm("Would you like uppercase letters in your password? ");
-if (!upperChoice) { // sets a return for the cancel button 
-  return;
+  //concats the individual arrays to make a new array that will have all available charaters in one array
+  if (!upperChoice && !lowerChoic && !numberChoice && !specialChoice) {
+    return;
+  }
+  if (upperChoice) {
+    currentUserArray = currentUserArray.concat(upperSelection);
+  }
+  if (lowerChoice) {
+    currentUserArray = currentUserArray.concat(lowerSelection);
+  }
+  if (numberChoice) {
+    currentUserArray = currentUserArray.concat(numberSelection);
+  }
+  if (specialChoice) {
+    currentUserArray = currentUserArray.concat(specialCharSelection);
+  }
+  console.log(currentUserArray);
+
+  for (let index = 0; index < passwordLength; index++) {
+    //uses the array to randomize the password
+    let randomNumber = Math.floor(Math.random() * currentUserArray.length);
+    let randomChar = currentUserArray[randomNumber];
+
+    passWord = passWord.concat(randomChar);
+  }
+  return passWord;
 }
-
-let lowerChoice = confirm("Would you like lowercase letters in your password? ");
-if (!lowerChoice) {
-  return;
-}
-
-let numberChoice = confirm("Would you like numbers in your password? ");
-if (!numberChoice) {
-  return;
-}
-
-let specialChoice = confirm("Would you like special characters in your password? ");
-if (!specialChoice) {
-  return;
-}
-
-//concats the individual arrays to make a new array that will have all available charaters in one array
-if (upperChoice){
-  currentUserArray = currentUserArray.concat(upperSelection);
-}
-if (lowerChoice){
-  currentUserArray = currentUserArray.concat(lowerSelection);
-}
-if (numberChoice){
-  currentUserArray = currentUserArray.concat(numberSelection);
-}
-if (specialChoice){
-  currentUserArray = currentUserArray.concat(specialCharSelection);
-}
-
-
-
-
-
-for (let index = 0; index < passwordLength; index++) { //uses the array to randomize the password
-  finalArray =  Math.floor(Math.random() * currentUserArray.length);
-  finalArray = currentUserArray[finalArray];
-  passWord = passWord.concat(finalArray);
-  console.log(finalArray);
-}
-return passWord;
-
-}
-
 
 // Write password to the #password input
 
@@ -84,7 +150,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
